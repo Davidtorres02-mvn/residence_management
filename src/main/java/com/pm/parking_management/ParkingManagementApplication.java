8f035c93fb1dd476b4af4930ed45cfcf45195465
Davidtorres07-py <hernandeztorresd65@gmail.com>
2024-06-06 19:14:32 -0600

    proyect setup

---
+++ src/main/java/com/pm/parking_management/ParkingManagementApplication.java
@@ -0,0 +1,14 @@
+package com.pm.parking_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ParkingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagementApplication.class, args);
+	}
+
+}
